acc17f64e447e73f64b858cef2d9f95e1d2af4ac
ramonreyes-create <ramon.reyes@csuv.cl>
2026-05-16 22:31:50 -0400

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
